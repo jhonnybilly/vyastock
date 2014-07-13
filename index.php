1a602220cc5e4c54283ccc2c368fddef7f016186
jhonnybilly <codeandolo@hotmail.com>
2014-07-13 00:35:52 -0500

    Ingreso de Cabecera
    añadir meta y componentes para el head

--- index.php
+++ index.php
@@ -2,9 +2,26 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Documento sin título</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>V&A Servicios y Representaciones</title>
+<link rel="stylesheet" href="stylus/base.css" type="text/css">
+<link rel="shortcut icon" href="recursos/icono.ico">
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/feed" />
+<meta name="description" content="Venta de Equipos Informaticos, Electrodomesticos ente otros dispositivos garantizados, brindando un servicio de primera.">
+<meta content="Jhonny Miñan" name="author" />
+<meta content="informatica,ventas,electrodomesticos,computadoras,tablet" name="keywords" />
+<!-- og tags -->
+<meta property="og:description" content="Venta de Equipos Informaticos, Electrodomesticos ente otros dispositivos garantizados, brindando un servicio de primera." />
+<meta property="og:title" content="Almacen V&A: Venta de Equipos Informaticos" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="almacenvya.com"/>
+<!-- og tags google+ -->
+<meta itemprop="description" content="Venta de Equipos Informaticos, Electrodomesticos ente otros dispositivos garantizados, brindando un servicio de primera.">
+<script type="text/javascript" src="efect/base.js"></script>
 </head>
-
 <body>
+
+
+
 </body>
 </html>
